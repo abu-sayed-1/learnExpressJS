c442928c64e08ba61912fd8348347d8b51330697
as ahmed <rakibasrr13@gmail.com>
2021-08-09 12:05:21 +0300

    Dynamic Website using Express.js

---
+++ dynamicWebsiteUsingExpressjs/src/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+
+const app = express();
+const port = 4000;
+const partialsPath = path.join(__dirname, "../templates/partials");
+const templatePath = path.join(__dirname, "../templates/views");
+
+// to set the view engine
+app.set("view engine", "hbs");
+app.set("views", templatePath);
+hbs.registerPartials(partialsPath);
+
+app.get("/", (req, res) => {
+    res.render("index", {
+        authName: "auth: AS Rakib"
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact")
+});
+
+app.get("/blog", (req, res) => {
+    res.render("blog")
+})
+
+app.get("*", (req, res) => {
+    res.render("pageNotFound", {
+        error: "Oops Page not Found ):"
+    })
+})
+
+app.listen(port, () => {
+    console.log("server listen by 4000")
+})
